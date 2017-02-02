307c5f07aea59d665f3a176fd3429256b51f3830
Nick Coury <nickcoury@gmail.com>
2017-02-01 18:48:58 -0700

    Add linting.

--- alexa-app-router.js
+++ alexa-app-router.js
@@ -1,108 +1,108 @@
-var router = {};
+var router = {}
 
-router.addRouter = function(app, config, intents, routes) {
-    if (!app) return;
+router.addRouter = function (app, config, intents, routes) {
+  if (!app) return
 
-    app.$$launch = app.launch;
-    app.launch = launchHandler;
+  app.$$launch = app.launch
+  app.launch = launchHandler
 
-    app.$$intent = app.intent;
-    app.intent = intentHandler;
+  app.$$intent = app.intent
+  app.intent = intentHandler
 
-    registerIntents(intents);
-    registerRoutes(routes, config);
-    registerDefaultIntents(routes);
+  registerIntents(intents)
+  registerRoutes(routes, config)
+  registerDefaultIntents(routes)
 
-    function registerIntents(intents) {
-        if (typeof intents !== 'object') return;
+  function registerIntents (intents) {
+    if (typeof intents !== 'object') return
 
-        for (var name in intents) {
-            if (intents[name]) {
-                app.intent(name, intents[name], noop);
-            } else {
-                app.intent(name, noop);
-            }
-        }
+    for (var name in intents) {
+      if (intents[name]) {
+        app.intent(name, intents[name], noop)
+      } else {
+        app.intent(name, noop)
+      }
+    }
+  }
+
+  function registerRoutes (routes, config) {
+    app.$$routeList = routes || {}
+    app.$$routeConfig = config || {}
+
+    if (typeof app.$$routeConfig.pre === 'function') {
+      app.pre = app.$$routeConfig.pre
     }
 
-    function registerRoutes(routes, config) {
-        app.$$routeList = routes || {};
-        app.$$routeConfig = config || {};
-
-        if (typeof app.$$routeConfig.pre === 'function') {
-            app.pre = app.$$routeConfig.pre;
-        }
-
-        if (typeof app.$$routeConfig.post === 'function') {
-            app.post = app.$$routeConfig.post;
-        }
-
-        if (typeof app.$$routeConfig.launch === 'function') {
-            app.launch(app.$$routeConfig.launch);
-        }
+    if (typeof app.$$routeConfig.post === 'function') {
+      app.post = app.$$routeConfig.post
     }
 
-    function registerDefaultIntents(routes) {
-        for (var routeName in routes) {
-            for (var intentName in routes[routeName]) {
-                if (!app.intents[intentName]) {
-                    app.intent(intentName, noop);
-                }
-            }
-        }
+    if (typeof app.$$routeConfig.launch === 'function') {
+      app.launch(app.$$routeConfig.launch)
     }
+  }
 
-    function noop(request, response) {}
+  function registerDefaultIntents (routes) {
+    for (var routeName in routes) {
+      for (var intentName in routes[routeName]) {
+        if (!app.intents[intentName]) {
+          app.intent(intentName, noop)
+        }
+      }
+    }
+  }
 
-    function launchHandler(handler) {
-        app.$$launch(function (request, response) {
-            response.route = function (nextRouteName) {
-                response
+  function noop (request, response) {}
+
+  function launchHandler (handler) {
+    app.$$launch(function (request, response) {
+      response.route = function (nextRouteName) {
+        response
                     .shouldEndSession(false)
-                    .session('route', nextRouteName);
-                return response;
-            };
+                    .session('route', nextRouteName)
+        return response
+      }
 
-            return handler.apply(null, arguments);
-        });
+      return handler.apply(null, arguments)
+    })
+  }
+
+  function intentHandler (name, config, handler) {
+    if (typeof config === 'function') {
+      handler = config
+      config = null
     }
 
-    function intentHandler(name, config, handler) {
-        if (typeof config == "function") {
-            handler = config;
-            config = null;
-        }
+    app.$$intent(name, config, function (request, response) {
+      var routeName
+      routeName = request.session('route')
+      response.session('route', null)
 
-        app.$$intent(name, config, function (request, response) {
-            var routeName;
-            routeName = request.session('route');
-            response.session('route', null);
+      request.route = routeName
 
-            request.route = routeName;
+      response.route = function (nextRouteName) {
+        response
+          .shouldEndSession(false)
+          .session('route', nextRouteName)
+        return response
+      }
 
-            response.route = function(nextRouteName) {
-                response
-                    .shouldEndSession(false)
-                    .session('route', nextRouteName);
-                return response;
-            };
+      // Call route handler
+      var routeHandler = app.$$routeList[routeName]
+      var defaultRouteHandler = app.$$routeList[app.$$routeConfig.defaultRoute]
 
-            // Call route handler
-            var routeHandler = app.$$routeList[routeName];
-            var defaultRouteHandler = app.$$routeList[app.$$routeConfig.defaultRoute];
+      if (routeHandler && typeof routeHandler[name] === 'function') {
+        return routeHandler[name].apply(null, arguments)
+      } else if (defaultRouteHandler && typeof defaultRouteHandler[name] === 'function') {
+        return defaultRouteHandler[name].apply(null, arguments)
+      } else {
+        // Call original handler if no route matches
+        return handler.apply(null, arguments)
+      }
+    })
+  }
 
-            if (routeHandler && typeof routeHandler[name] === 'function') {
-                return routeHandler[name].apply(null, arguments);
-            } else if (defaultRouteHandler && typeof defaultRouteHandler[name] === 'function') {
-                return defaultRouteHandler[name].apply(null, arguments);
-            } else {
-                // Call original handler if no route matches
-                return handler.apply(null, arguments);
-            }
-        });
-    }
+  return app
+}
 
-    return app;
-};
-
-module.exports = router;
+module.exports = router
