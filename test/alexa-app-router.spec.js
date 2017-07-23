ca142688217477481e11f02f939aff7d0462eb7d
Nick Coury <nickcoury@users.noreply.github.com>
2017-07-23 16:44:08 -0700

    Improve routing paradigm (#9)

--- test/alexa-app-router.spec.js
+++ test/alexa-app-router.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable handle-callback-err, no-undef */
 var _ = require('lodash');
 var chai = require('chai');
 var sinon = require('sinon');
@@ -13,9 +14,20 @@ var intents;
 var routes;
 
 describe('alexa-app-router', function() {
+  var exitHandler, helpHandler, testHandler;
+  var event, context, callback;
+
   beforeEach(function() {
+    exitHandler = sinon.stub();
+    helpHandler = sinon.stub();
+    testHandler = sinon.stub();
+
     config = {
-      defaultRoute: '/',
+      defaultRoutes: {
+        'AMAZON.CancelIntent': '/exit',
+        'AMAZON.HelpIntent': '/help',
+        TestIntent: '/test'
+      },
       pre: sinon.stub(),
       launch: sinon.stub()
     };
@@ -25,14 +37,10 @@ describe('alexa-app-router', function() {
     };
 
     routes = {
-      '/': {
-        'AMAZON.HelpIntent': sinon.stub(),
-        TestIntent: sinon.stub()
-      },
-      '/detail': {
-        'AMAZON.NoIntent': sinon.stub(),
-        'AMAZON.YesIntent': sinon.stub()
-      }
+      '/exit': exitHandler,
+      '/help': helpHandler,
+      '/test': testHandler,
+      '/test/{testId}': testHandler
     };
 
     delete require.cache[require.resolve('alexa-app')];
@@ -40,6 +48,56 @@ describe('alexa-app-router', function() {
     app = new alexaApp.app('alexa-app-router-app');
 
     router = require('../alexa-app-router');
+
+    event = {
+      session: {
+        new: false,
+        sessionId: 'amzn1.echo-api.session.[unique-value-here]',
+        attributes: {},
+        user: {
+          userId: 'amzn1.ask.account.[unique-value-here]'
+        },
+        application: {
+          applicationId: 'amzn1.ask.skill.[unique-value-here]'
+        }
+      },
+      version: '1.0',
+      request: {
+        locale: 'en-US',
+        timestamp: '2016-10-27T21:06:28Z',
+        type: 'IntentRequest',
+        requestId: 'amzn1.echo-api.request.[unique-value-here]',
+        intent: {
+          slots: {
+            Item: {
+              name: 'TestItem',
+              value: '123'
+            }
+          },
+          name: 'TestIntent'
+        }
+      },
+      context: {
+        AudioPlayer: {
+          playerActivity: 'IDLE'
+        },
+        System: {
+          device: {
+            supportedInterfaces: {
+              AudioPlayer: {}
+            }
+          },
+          application: {
+            applicationId: 'amzn1.ask.skill.[unique-value-here]'
+          },
+          user: {
+            userId: 'amzn1.ask.account.[unique-value-here]'
+          }
+        }
+      }
+    };
+    context = {};
+    callback = sinon.stub();
 
     sinon.spy(app, 'intent');
   });
@@ -79,6 +137,81 @@ describe('alexa-app-router', function() {
       app = router.addRouter(app, config, intents, routes);
 
       expect(app.$$intent).to.have.been.called;
+    });
+  });
+
+  describe('called with a route name', function() {
+    beforeEach(function() {
+      event.session.attributes.route = {
+        TestIntent: '/help'
+      };
+      event.request.intent.name = 'TestIntent';
+    });
+
+    it('should call the appropriate route', function(done) {
+      app = router.addRouter(app, config, intents, routes);
+      app.handler(event, context, function(err, result) {
+        expect(testHandler).to.not.have.been.called;
+        expect(helpHandler).to.have.been.calledWithMatch({
+          route: {
+            params: {},
+            query: {},
+            route: '/help',
+            url: '/help'
+          }
+        });
+        done();
+      });
+    });
+  });
+
+  describe('called with an intent with no route', function() {
+    beforeEach(function() {
+      event.request.intent.name = 'NoRouteIntent';
+    });
+
+    it('should throw an error', function(done) {
+      app = router.addRouter(app, config, intents, routes);
+      app.handler(event, context, function(err, result) {
+        expect(helpHandler).to.have.been.calledWithMatch({
+          route: {
+            params: {},
+            query: {},
+            route: '/help',
+            url: '/help'
+          }
+        });
+        done();
+      });
+    });
+  });
+
+  describe('called with query and route parameters', function() {
+    beforeEach(function() {
+      event.session.attributes.route = {
+        TestIntent: '/test/123?parameter=456&parameter2=789'
+      };
+      event.request.intent.name = 'TestIntent';
+    });
+
+    it('should load them correctly', function(done) {
+      app = router.addRouter(app, config, intents, routes);
+      app.handler(event, context, function(err, result) {
+        expect(testHandler).to.have.been.calledWithMatch({
+          route: {
+            params: {
+              testId: '123'
+            },
+            query: {
+              parameter: '456',
+              parameter2: '789'
+            },
+            route: '/test/{testId}',
+            url: '/test/123?parameter=456&parameter2=789'
+          }
+        });
+        done();
+      });
     });
   });
 });
